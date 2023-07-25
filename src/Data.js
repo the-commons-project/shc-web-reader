14b99e0cf57071963a4160122216179e9f1610d8
Sean Nolan <sean@thenolans.com>
2023-07-25 16:22:12 -0700

    woo hoo fhir me up

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, TextField, Select, MenuItem } from '@mui/material';
+import { useOptionalFhir } from './OptionalFhir';
 import { verifySHX, SHX_STATUS_NEED_PASSCODE, SHX_STATUS_OK  } from './lib/SHX.js';
-import { saveDivToFile } from './lib/saveDiv.js';
+import { saveDivToFile, saveDivToFHIR } from './lib/saveDiv.js';
 import * as res from './lib/resources.js';
 import ValidationInfo from './ValidationInfo.js';
 
@@ -14,6 +15,8 @@ export default function Data({ shx }) {
   const [shxResult, setShxResult] = useState(undefined);
   const [bundleIndex, setBundleIndex] = useState(0);
   const [showSource, setShowSource] = useState(false);
+
+  const fhir = useOptionalFhir();
 
   // +--------------------+
   // | renderNeedPasscode |
@@ -78,9 +81,9 @@ export default function Data({ shx }) {
   const renderBundle = () => {
 
 	const bundle = shxResult.bundles[bundleIndex];
-	const organized = bundle.organized;
+	const organized = (bundle.contentOK() ? bundle.organized : undefined);
 	
-	let elt = <></>;
+	let elt = undefined;
 
 	if (organized) {
 	  
@@ -104,12 +107,15 @@ export default function Data({ shx }) {
 	
 	return(
 	  <>
-		<ValidationInfo bundle={bundle} />
 		{ renderBundleChooser() }
-		<div id="bundle-contents">{ elt }</div>
+		<div id="bundle-contents">
+		  <ValidationInfo bundle={bundle} />
+		  { elt }
+		</div>
 		<div>
 	      <Button onClick={ () => setShowSource(!showSource) }>source</Button>
-	      <Button onClick={ onSaveClick }>save</Button>
+	      { elt && <Button onClick={ () => onSaveClick(true) }>save to file</Button> }
+	      { elt && fhir && <Button onClick={ () => onSaveClick(false) }>save to ehr</Button> }
 	      { showSource && <pre><code>{JSON.stringify(bundle, null, 2)}</code></pre>}
 		</div>
 	  </>
@@ -117,7 +123,7 @@ export default function Data({ shx }) {
 	
   }
 
-  const onSaveClick = () => {
+  const onSaveClick = (toFile) => {
 
 	// defensive because we can show in error cases too
 	const baseName = (shxResult &&
@@ -127,8 +133,15 @@ export default function Data({ shx }) {
 					  shxResult.bundles[bundleIndex].organized.label
 					  ? shxResult.bundles[bundleIndex].organized.label
 					  : "Shared Information");
-					  
-	saveDivToFile(document.getElementById("bundle-contents"), baseName);
+
+	const div = document.getElementById("bundle-contents");
+	
+	if (toFile) {
+	  saveDivToFile(div, baseName);
+	}
+	else {
+	  saveDivToFHIR(fhir, div, baseName);
+	}
   }
 
   const onBundleChange = (evt) => {
--- src/lib/saveDiv.js
+++ src/lib/saveDiv.js
@@ -1,28 +1,106 @@
 
 import html2canvas from 'html2canvas';
 
-// +-------------+
-// | divToCanvas |
-// +-------------+
-
-export async function divToCanvas(div) {
-  return(await html2canvas(div));
-}
-
 // +---------------+
 // | saveDivToFile |
 // +---------------+
 
 export async function saveDivToFile(div, baseName) {
 
-  const canvas = await divToCanvas(div);
-  const link = document.createElement("a");
-  link.href = canvas.toDataURL("image/jpeg", 1.0);
-
-  link.download = todayForFilename() + ' ' + baseName + '.' +
-	(link.href.startsWith("data:image.png") ? "png" : "jpg");
+  const imageInfo = await divToImage(div);
   
+  const link = document.createElement("a");
+  link.href = imageInfo.url;
+  link.download = getFilename(baseName, imageInfo.extension);
+
   link.click();
+}
+
+// +---------------+
+// | saveDivToFHIR |
+// +---------------+
+
+export async function saveDivToFHIR(fhir, div, baseName) {
+
+  const imageInfo = await divToImage(div);
+  const now = new Date().toISOString();
+
+  let patientReference = fhir.patient.id;
+  if (!patientReference.startsWith("Patient/")) {
+	patientReference = "Patient/" + patientReference;
+  }
+
+  let userReference = fhir.user.fhirUser;
+  if (!userReference.startsWith(fhir.user.resourceType)) {
+	userReference = fhir.user.resourceType + "/" + userReference;
+  }
+
+  const documentReference = {
+	"resourceType": "DocumentReference",
+	"status": "current",
+	"docStatus": "final",
+	"type": { "text": "Scanned SMART Health Link or Card" },
+	"subject": {
+	  "type": "Patient",
+	  "reference": patientReference
+	},
+	"author": [ {
+	  "type": fhir.user.resourceType,
+	  "reference": userReference
+	} ],
+	"context": { "period": { "start": now  } },
+	"content": {
+	  "attachment": {
+		"contentType": imageInfo.contentType,
+		"data": imageInfo.base64,
+		"title": getFilename(baseName, imageInfo.extension),
+		"width": imageInfo.width,
+		"height": imageInfo.height,
+		"creation": now
+	  }
+	}
+  }
+
+  try {
+	const response = await fhir.create(documentReference);
+	if (response.id) alert("View saved to patient record");
+  }
+  catch (e) {
+	alert(e);
+  }
+
+}
+
+// +---------+
+// | Helpers |
+// +---------+
+
+async function divToImage(div) {
+  const canvas = await html2canvas(div);
+
+  const info = {
+	url: canvas.toDataURL("image/jpeg", 1.0),
+	width: canvas.width,
+	height: canvas.height
+  };
+
+  if (!info.url.startsWith("data:")) return(info);
+
+  // we're a little lazy about optional components because we know
+  // that toDataURL returns the media type and base64 tag
+
+  const ichSemi = info.url.indexOf(";");
+  const ichComma = info.url.indexOf(",");
+
+  info.contentType = info.url.substring(5, ichSemi);
+  info.extension = (info.contentType === "image/png" ? "png" : "jpg");
+  info.base64 = info.url.substring(ichComma + 1);
+
+  return(info);
+}
+
+function getFilename(baseName, ext) {
+  return(todayForFilename() + ' ' + baseName + '.' + ext);
 }
 
 function todayForFilename() {
@@ -37,3 +115,5 @@ function todayForFilename() {
 
   return([now.getFullYear(), month, day].join('-'));
 }
+
+
