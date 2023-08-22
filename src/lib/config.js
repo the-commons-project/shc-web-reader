11a85fcae2a49b45dcd89af19d95e24b76c4058e
Sean Nolan <sean@thenolans.com>
2023-08-22 10:27:59 -0700

    domain config overrides for prod

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,13 +1,18 @@
 
-import { DEFAULT_CONFIG } from './defaults.js';
+import { DEFAULT_CONFIG, DOMAIN_OVERRIDES } from './defaults.js';
 
 let _cfg = undefined;
+
+export default function config(key) {
+  return(getConfig()[key]);
+}
 
 export function getConfig() {
 
   if (_cfg) return(_cfg);
   
   const cfg = DEFAULT_CONFIG;
+  applyDomainOverrides(cfg);
 
   // TODO - NYI override config for SMART on FHIR launch
   const overrideSource = document.location.search;
@@ -29,6 +34,20 @@ export function getConfig() {
   return(_cfg);
 }
 
-export default function config(key) {
-  return(getConfig()[key]);
+function applyDomainOverrides(cfg) {
+
+  const host = window.location.hostname.toLowerCase();
+
+  Object.keys(DOMAIN_OVERRIDES).forEach((domain, i) => {
+	
+	if (host.indexOf(domain.toLowerCase()) !== -1) {
+
+	  console.log(`Using config overrides for ${domain}`);
+	  const overrides = DOMAIN_OVERRIDES[domain];
+
+	  Object.keys(overrides).forEach((key, j) => {
+		cfg[key] = overrides[key];
+	  });
+	}
+  });
 }
