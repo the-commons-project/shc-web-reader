37ba063a6bb03789f81bf2ed01242739f97aa319
Sean Nolan <sean@thenolans.com>
2023-11-10 09:18:56 -0800

    per jp email

--- src/lib/defaults.js
+++ src/lib/defaults.js
@@ -47,7 +47,6 @@ export const DOMAIN_OVERRIDES = {
 	
 	"showScan": false,
 	"showSearch": false,
-	"showFile": false,
 	
 	"trustedDirectories": [
 	  'https://raw.githubusercontent.com/the-commons-project/vci-directory/main/logs/vci_snapshot.json'
