b44a78752924e8cd513fc5b33acb2183d1a122bc
Sean Nolan <sean@thenolans.com>
2026-04-24 22:49:20 -0700

    missing file

---
+++ src/Collection.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import * as futil from './lib/fhirUtil.js';
+import * as ftabs from './lib/fhirTables.js';
+import styles from './PatientSummary.module.css';
+import { useLanguage } from './lib/LanguageContext';
+import { useDocumentModal } from './DocumentModalContext';
+
+export default function Collection({ organized, dcr }) {
+
+  const { t } = useLanguage();
+  const { showDocuments } = useDocumentModal();
+
+  // +-------------+
+  // | Main Render |
+  // +-------------+
+  
+  const tableState = {};
+  for (const i in organized.all) ftabs.addResource(organized.all[i], tableState, organized);
+  const jsx = ftabs.renderJSX(tableState, styles.fhirTable, organized, { dcr, loc: t, doc: showDocuments });
+  
+  return(<>{jsx}</>);
+}
+
+  
